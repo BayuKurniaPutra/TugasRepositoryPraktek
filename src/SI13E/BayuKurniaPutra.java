ce7793e3dd252c5ac9d4218e41ed04b80c6720ee
BayuKurniaPutra <Basink.bayu@gmail.com>
2014-10-07 00:49:48 +0700

    versi 2.3.2

---
+++ src/SI13E/BayuKurniaPutra.java
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+package SI13E;
+
+
+
+
+
+
+
+public class BayuKurniaPutra {
+
+        
+    
+     
+     
+    public static void main(String[] args) {
+    String Nama = ("Bayu Kurnia Putra");
+    String Kelas = ("SI13E");
+    String Alamat = ("JL. Pulau Bangka Sukabumi, BandarLampung");
+    String Jurusan = ("Sistem Informasi");
+    
+    System.out.println("TEKNOKRAT");
+    System.out.println("Sang Juara\n");
+    
+            System.out.println("Nama    :  "+Nama); 
+            System.out.println("Kelas   :  "+Kelas);
+            System.out.println("Alamat  :  "+Alamat);
+            System.out.println("Jurusan :  "+Jurusan);
+    System.out.println("\nTUGAS UPLOAD REPOSITORY !!!");
+    }
+    
+}
